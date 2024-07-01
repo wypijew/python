c90be191fb5ab8831803537a1170ac912958aa31
Ola Wypijewska <Ola.Wypijewska@gmail.com>
2024-07-01 03:30:04 +0100

    day 1

---
+++ day1_exercise_main.py
@@ -0,0 +1,20 @@
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# print(len(input("What is your name?\n")))
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+name = input("What is your name?\n")
+print(name)
+
+print(input("What is your name?\n"))
+
+
+print("She said: \"Hello\" and then left.") #a way to use two double quotes in one string
+
+
